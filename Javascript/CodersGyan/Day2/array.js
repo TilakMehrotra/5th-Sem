0985df2aaecf216843589d9312ceceabe1daf3d2
TilakMehrotra <42639029+TilakMehrotra@users.noreply.github.com>
2023-10-19 00:34:34 +0530

    Add files via upload

---
+++ Javascript/CodersGyan/Day2/array.js
@@ -0,0 +1,39 @@
+// Arrays and loops
+    // const languages = ["javascript", "python", "c"];
+    // length 
+    // console.log(languages.length);
+    // console.log(languages[1]); // we can get item using idex from array
+
+    // to add an item to an array
+    // languages.push("dart"); // push adds to the last of the array   
+    // languages.unshift("java"); // it adds an item first in the array
+    // languages.pop(); // it removes the last item from the array
+    // languages.shift(); // it removes the first item from the array
+
+    // console.log(languages);
+
+    // for(let i = 0; i < 4; i++) {
+    //     console.log("t");
+    // }
+
+    // const actors = [ // we store three objects in an array
+    //     {
+    //         this: "actor 1",
+    //         payment: 100
+
+    //     },
+    //     {
+    //         this: "actor 2",
+    //         payment: 50
+
+    //     },
+    //     {
+    //         this: "actor 3",
+    //         payment: 150
+
+    //     }
+    // ];
+
+    // for (let i = 0; i < actors.length; i++ ) {
+    //     console.log(actors[i]);
+    // }
